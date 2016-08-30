716fb77b05ff3e7060125b57be54b336204a448e
NivenT <nachenjang@gmail.com>
2016-08-30 14:36:36 -0400

    Improved region detection

--- src/main.cpp
+++ src/main.cpp
@@ -17,18 +17,33 @@ bool line_contains_blocks(const CImg<unsigned char>& red_channel, const CImg<uns
         return blue >= 200;
     });
 
-    return red_count >= 400 && blue_count <= 100 && green_count <= 200;
+    return red_count >= .26*red_channel.width() && blue_count <= .13*blue_channel.width() && green_count <= .26*green_channel.width();
+}
+
+bool line_is_blank(const CImg<unsigned char>& red_channel, const CImg<unsigned char>& green_channel, const CImg<unsigned char>& blue_channel) {
+    static const int threshold = .91*red_channel.width();
+    auto red_count = std::count_if(red_channel.begin(), red_channel.end(), [](auto red) {
+        return 60 <= red && red <= 80;
+    });
+    auto green_count = std::count_if(green_channel.begin(), green_channel.end(), [](auto green) {
+        return 30 <= green && green <= 50;
+    });
+    auto blue_count = std::count_if(blue_channel.begin(), blue_channel.end(), [](auto blue) {
+        return blue <= 20;
+    });
+
+    return red_count >= threshold && blue_count >= threshold && green_count >= threshold;
 }
 
 std::tuple<int, int, int, int> get_game_region(const CImg<unsigned char>& img) {
-    int y_start = 0, y_end = img.height()-1;
+    int y_start = -1, y_end = img.height()-1;
     for (std::size_t line = 0; line < img.height(); ++line) {
         auto red_channel = img.get_crop(0, line, 0, 0, img.width()-1, line, 0, 0);
         auto green_channel = img.get_crop(0, line, 0, 1, img.width()-1, line, 0, 1);
         auto blue_channel = img.get_crop(0, line, 0, 2, img.width()-1, line, 0, 2);
 
-        if (line_contains_blocks(red_channel, green_channel, blue_channel)) {
-            if (y_start == 0) {
+        if (line_contains_blocks(red_channel, green_channel, blue_channel) || line_is_blank(red_channel, green_channel, blue_channel)) {
+            if (y_start == -1) {
                 y_start = line;
             } else {
                 y_end = line;
@@ -36,14 +51,14 @@ std::tuple<int, int, int, int> get_game_region(const CImg<unsigned char>& img) {
         }
     }
 
-    int x_start = 0, x_end = img.width()-1;
+    int x_start = -1, x_end = img.width()-1;
     for (std::size_t column = 0; column < img.width(); ++column) {
         int red = img(column, y_start, 0, 0);
         int green = img(column, y_start, 0, 1);
         int blue = img(column, y_start, 0, 2);
 
         if (red + green + blue < 200) {
-            if (x_start == 0) {
+            if (x_start == -1) {
                 x_start = column;
             } else {
                 x_end = column;
@@ -51,18 +66,20 @@ std::tuple<int, int, int, int> get_game_region(const CImg<unsigned char>& img) {
         }
     }
 
-    return std::make_tuple(x_start, y_start, x_end, y_end);
+    return std::make_tuple(std::max(0, x_start), std::max(0, y_start), x_end, y_end);
 }
 
 int main() {
-    CImg<unsigned char> image("puzzles/images/puzzle1.jpg"), visu(500,400,1,3,0);
+    CImg<unsigned char> image("puzzles/images/puzzle3.png"), visu(500,400,1,3,0);
+
+    std::cout<<"Image dimensions: "<<image.width()<<" X "<<image.height()<<std::endl;
+
     const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-    const unsigned char white[] = {255, 255, 255}, black[] = {0, 0, 0};
+    const unsigned char white[] = {255, 255, 255};
 
     int x1, y1, x2, y2;
-    std::tie(x1, y1, x2, y2) = get_game_region(image.get_blur(2.5));
-    //image.draw_rectangle(x1, y1, x2, y2, white, 1, ~0);
-    image.crop(x1, y1, x2, y2);
+    std::tie(x1, y1, x2, y2) = get_game_region(image.get_blur(1.5));
+    image.draw_rectangle(x1, y1, x2, y2, white, 1, ~0);
 
     CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
     while (!main_disp.is_closed() && !draw_disp.is_closed()) {
